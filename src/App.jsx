fbb7c972c91910ccc978f4aa46de5f32529bc23c
SaraToth <saratoth101@gmail.com>
2025-03-25 19:14:13 +0900

    Feat: Create GenInfo form and update App/main
    Create the GenInfo form component and render it via App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
+import GenInfo from './components/GenInfo'
 
 function App() {
-
+  return(
+    <GenInfo />
+  )
 }
 
 export default App
---
+++ src/components/GenInfo.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react"
+
+export default function GenInfo() {
+    const [genInfo, setGenInfo] = useState(
+        {
+            first: "",
+            last: "",
+            email: "",
+            phone: "",
+            title: "",
+        },
+    )
+    
+    const handleChange = (e) => {
+        const newFormData = {
+            ...genInfo,
+            [e.target.name]: e.target.value,
+        }
+        setGenInfo(newFormData);
+    }
+
+
+    const submitGenInfo = (e) => {
+        e.preventDefault();
+        const newFormData = {
+            ...genInfo,
+            [e.target.name]: e.target.value,
+        }
+
+        setGenInfo(newFormData);
+        console.log(JSON.stringify(genInfo));
+    }
+
+    return (
+        <div className="form-container-geninfo">
+            <form onSubmit={submitGenInfo}>
+                <h1>General Information</h1>
+                <div className="form-content">
+
+                    <div className="form-row">
+                        <div className="form-item">
+                            <label htmlFor="first">First Name:</label>
+                            <input type="text" id="first" name="first" value={genInfo.first} onChange={handleChange} required></input>
+                        </div>
+
+                        <div className="form-item">
+                            <label htmlFor="last">Last Name:</label>
+                            <input type="text" id="last" name="last" value={genInfo.last} onChange={handleChange} required></input>
+                        </div>
+                    </div>
+
+                    <div className="form-row">
+                        <div className="form-item">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" id="email" name="email" value={genInfo.email} onChange={handleChange} required></input>
+                        </div>
+
+                        <div className="form-item">
+                            <label htmlFor="phone">Phone:</label>
+                            <input type="phone" id="phone" name="phone" value={genInfo.phone} onChange={handleChange} required></input>
+                        </div>
+                    </div>
+
+
+                    <div className="form-item">
+                        <label htmlFor="title">Job Title:</label>
+                        <input type="text" id="title" name="title" value={genInfo.title} onChange={handleChange} required></input>
+                    </div>
+                </div>
+                <div className="form-btn">
+                    <button type="submit" className="submit-btn">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
