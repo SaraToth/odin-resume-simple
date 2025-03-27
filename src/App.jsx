b2f5271996fcbefaadfa96761528275c1ddbe8ed
SaraToth <saratoth101@gmail.com>
2025-03-27 20:49:25 +0900

    Feat: Remove print preview and swap with direction
    Print preview buttons are not necessary or that helpful if the user just opens the print preview screen. Removed this feature and swapped it out with a few lines of text to guide the user how to use the form, which disappears when the user goes to print.

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,20 @@ import GenInfo from './components/GenInfo'
 import Summary from './components/Summary'
 import Experience from './components/Experience'
 import Education from './components/Education'
-import { useState } from 'react'
+
 
 function App() {
-  const [printPreview, setPrintPreview] = useState(false);
-
-  const printPreviewOn = () => {
-    setPrintPreview(true);
-  }
-
-  const printPreviewOff = () => {
-    setPrintPreview(false);
-  }
 
   return(
-    <div className={`resume ${printPreview ? 'print-mode' : ''}`}>
+    <div className={`resume`}>
         <GenInfo />
         <Summary />
         <Experience />
         <Education />
-        <div className="print-btn-container">
-        {(!printPreview) && <button type='submit' onClick={printPreviewOn} className='btn print-btn'>Print Preview</button>}
-        {printPreview && <button type="submit" onClick={printPreviewOff} className='btn print-btn'>Close Print Preview</button>}
+        <div className="print-preview">
+          <p>Fill out all form fields</p>
+          <p>Press command p to open the print preview screen to see the final version of this resume</p>
+          <p>Save as a pdf file to save the final rendered resume</p>
         </div>
     </div>
   )
